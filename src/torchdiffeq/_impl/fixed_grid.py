92dad59b4cb3a37d49bab3ec4a0b3f4c045a4e8a
nikola-tesla17 <70263751+nikola-tesla17@users.noreply.github.com>
2022-07-29 17:19:31 +0530

    Update fixed_grid.py

--- src/torchdiffeq/_impl/fixed_grid.py
+++ src/torchdiffeq/_impl/fixed_grid.py
@@ -31,6 +31,6 @@ class RK4(FixedGridODESolver):
 class GGear2(FixedGridODESolver):
     order = 2
 
-    def _step_func(self, func, t0, dt, t1, y0):
-        f0 = func(t0, y0, perturb=Perturb.NEXT if self.perturb else Perturb.NONE)
-        return dt * f0, f0
+   # def _step_func(self, func, t0, dt, t1, y0):
+    #    f0 = func(t0, y0, perturb=Perturb.NEXT if self.perturb else Perturb.NONE)
+     #   return dt * f0, f0
