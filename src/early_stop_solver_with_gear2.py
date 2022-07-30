cefe4b8a4ff1401a292b93d2374a5db911409318
nikola-tesla17 <70263751+nikola-tesla17@users.noreply.github.com>
2022-07-30 19:17:59 +0530

    Update early_stop_solver_with_gear2.py

--- src/early_stop_solver_with_gear2.py
+++ src/early_stop_solver_with_gear2.py
@@ -253,7 +253,7 @@ class Gear2(FixedGridODESolver):
     self.no_alpha_sigmoid=False
     self.alpha_train = nn.Parameter(torch.tensor(0.2))
     self.dataset = opt['dataset']
-    self. att_opt = {'dataset': self.dataset, 'self_loop_weight': 1, 'leaky_relu_slope': 0.2, 'beta_dim': 'vc', 'heads': 2, 'K': 10, 'attention_norm_idx': 0, 'add_source':False, 'alpha_dim': 'sc', 'beta_dim': 'vc', 'max_nfe':1000, 'mix_features': False}
+    self. att_opt = {'dataset': self.dataset, 'self_loop_weight': 1, 'leaky_relu_slope': 0.2, 'beta_dim': 'vc', 'heads': 1, 'K': 10, 'attention_norm_idx': 0, 'add_source':False, 'alpha_dim': 'sc', 'beta_dim': 'vc', 'max_nfe':1000, 'mix_features': False}
     #self.multihead_att_layer = SpGraphAttentionLayer(in_features, out_features, self.att_opt, self.device).to(device)
     
     if opt['dataset'] == 'ogbn-arxiv':
