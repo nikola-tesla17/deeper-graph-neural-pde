871cd4c19fff8f937063d386e6ba2108e61c2068
nikola-tesla17 <70263751+nikola-tesla17@users.noreply.github.com>
2022-07-31 12:36:24 +0530

    Update early_stop_solver_with_gear2.py

--- src/early_stop_solver_with_gear2.py
+++ src/early_stop_solver_with_gear2.py
@@ -312,7 +312,7 @@ class Gear2(FixedGridODESolver):
 
     self.multihead_att_layer = SpGraphAttentionLayer(in_features, out_features, self.att_opt, self.device).to(device)
     print(self.data.x.shape)
-    attention, wx = self.multihead_att_layer(self.data.x, self.edge_index)
+    attention, wx = self.multihead_att_layer(self.data.x, self.data.edge_index)
     a = self.multiply_attention(self.data.x, attention, wx)
     t0 = t[0]
     t1 = t[-1]
